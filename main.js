5699e16e5e15d5e4e9b4126e2e7f82396e3f5213
rafajr91 <rah5024@gmail.com>
2017-09-11 12:53:11 -0700

    Stopwatch can be started when clicking start. #1

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 
-var $start = getElementById('start')
-$start.addEventListener('click', function startTimer () {
+var $startButton = document.getElementById('start')
+var $seconds = document.querySelector('.stopwatch')
+var seconds = 0
 
+function startSeconds() {
+  seconds = seconds + 1
 }
+
+function drawSeconds() {
+  if (seconds >= 0) {
+    $seconds.textContent = seconds
+  }
+}
+
+$startButton.addEventListener('click', function() {
+  setInterval(drawSeconds, 500)
+  setInterval(startSeconds, 1000)
+})
